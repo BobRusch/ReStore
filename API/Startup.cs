277f67c741b52f6503cd08170ea64e7efba02802
Bob Rusch <git-commit-address>
2022-01-13 10:58:51 -0500

    End of Section 9

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,66 +1,118 @@
+using System.Collections.Generic;
+using System.Text;
 using API.Data;
+using API.Entities;
 using API.middleware;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
 {
   public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI .NET 6", Version = "v1" });
-            });
-            services.AddDbContext<StoreContext>(opt =>
-            {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
 
-      app.UseMiddleware<ExceptionMiddleware>();
-      
-      if (env.IsDevelopment())
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
       {
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI .NET 6 v1"));
-      }
-
-      //app.UseHttpsRedirection();
-
-      app.UseRouting();
-
-      app.UseCors(opt => {
-        opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+          c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI .NET 6", Version = "v1" });
+          c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+          {
+              Description = "Jwt auth header",
+              Name = "Authorization",
+              In = ParameterLocation.Header,
+              Type = SecuritySchemeType.ApiKey,
+              Scheme = "Bearer"
+          });
+          c.AddSecurityRequirement(new OpenApiSecurityRequirement
+          {
+              {
+                  new OpenApiSecurityScheme
+                  {
+                      Reference = new OpenApiReference
+                      {
+                          Type = ReferenceType.SecurityScheme,
+                          Id = "Bearer"
+                      },
+                      Scheme = "oauth2",
+                      Name = "Bearer",
+                      In = ParameterLocation.Header
+                  },
+                  new List<string>()
+              }
+          });
       });
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
+      services.AddDbContext<StoreContext>(opt =>
       {
-        endpoints.MapControllers();
+          opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
+      services.AddCors();
+      services.AddIdentityCore<User>(opt=>{
+        opt.User.RequireUniqueEmail = true;
+      })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<StoreContext>();
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(opt =>
+        {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                    .GetBytes(Configuration["JWTSettings:TokenKey"]))
+            };
+        });
+      services.AddAuthentication();
+      services.AddAuthorization();
+      services.AddScoped<TokenService>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+        app.UseMiddleware<ExceptionMiddleware>();
+        
+        if (env.IsDevelopment())
+        {
+          app.UseSwagger();
+          app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI .NET 6 v1"));
+        }
+
+        //app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors(opt => {
+          opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+        });
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+          endpoints.MapControllers();
+        });
     }
   }
 }
